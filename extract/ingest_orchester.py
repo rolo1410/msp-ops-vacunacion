936f6f62aa9ecdaa1f85158775739cab4fd3c581
rolando.casigna <rolo4336@gmail.com>
2025-10-01 20:38:23 +0000

    FEAT: Se añade la función para fusionar archivos parquet y se implementa la inserción de datos en DuckDB desde archivos parquet.

--- extract/ingest_orchester.py
+++ extract/ingest_orchester.py
@@ -5,7 +5,7 @@ from extract.db_vacunacion_rutinario import get_db_vacunaciones_parallel_rutinar
 from extract.geo_salud import get_geo_salud_data
 from extract.mpi import get_mpi_data
 from lake.init_lake import add_new_elements_to_lake
-from lake.load_lake import load_data
+from lake.load_lake import load_data,get_identificaciones_data
 
 
 def ingest_orchester(since, until, chunk_size=500000, max_workers=4, use_cache=True):
@@ -15,18 +15,18 @@ def ingest_orchester(since, until, chunk_size=500000, max_workers=4, use_cache=T
     logging.info("|- Usando versión paralela con persistencia automática")
     
     # La función ya no retorna DataFrame, persiste directamente en una base de datos duckdb
-    get_db_vacunaciones_parallel(since, until, chunk_size, max_workers)
+    # get_db_vacunaciones_parallel(since, until, chunk_size, max_workers)
     
     ## obtiene los datos de vacunación de rutina
     ##get_db_vacunaciones_parallel_rutinario(since, until, chunk_size, max_workers)
     
     # Cargar datos desde el lago para obtener las identificaciones
     logging.info("|- Cargando datos desde el lago para procesamiento posterior")
-    df = load_data()
+    df = get_identificaciones_data()
     
     # datos del registro civil
     logging.info("|- Procesando datos del registro civil (MPI)")
-    unique_identifiers = df['NUM_IDEN'].drop_nulls().drop_nans().unique().to_list()
+    unique_identifiers = df['num_iden'].drop_nulls().drop_nans().unique().to_list()
     logging.info(f" |- Total de identificaciones únicas: {len(unique_identifiers):,}")
     
     mpi_df = get_mpi_data(unique_identifiers)
--- lake/load_lake.py
+++ lake/load_lake.py
@@ -8,7 +8,18 @@ def load_data()-> pl.DataFrame:
     # Implement the logic to load data into the lake
     logging.info("|- Cargando datos al lago")
     con = duckdb.connect(database='./resources/data_lake/vacunacion.duckdb')
-    df = con.execute("SELECT * FROM lk_vacunacion").fetch_df()
+    df = con.execute("SELECT * FROM vacunacion").fetch_df()
+    df = pl.from_pandas(df)
+    logging.info(" |- Datos cargados al lago")
+    return df
+
+def get_identificaciones_data()-> pl.DataFrame:
+    # Implement the logic to load data into the lake
+    logging.info("|- Cargando datos al lago")
+    con = duckdb.connect(database='./resources/data_lake/vacunacion.duckdb')
+    df = con.execute("select distinct(v.num_iden) from vacunacion v ").fetch_df()
+    ## remover " y ' de las identificaciones :"
+    df['num_iden'] = df['num_iden'].str.replace(':','').str.replace('"','').str.replace("'",'')
     df = pl.from_pandas(df)
     logging.info(" |- Datos cargados al lago")
     return df
